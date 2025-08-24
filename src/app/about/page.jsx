1f0771196f04d92615b04fdb3db5de792b3c69cc
SADMAN-SADID-052 <sadman.cse8.bu@gmail.com>
2025-08-24 13:09:44 +0600

    Linking and Navigation

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,34 @@
+"use client"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 import React from "react";
 
 export default function AboutPage() {
+
+    const router = useRouter();
+    const isLoggedIn = true;
+    const handleNavigation = () =>{
+
+        if(isLoggedIn){
+            router.push("/about/address");
+
+        }
+
+        else{
+
+            router.push("/");
+        }
+    }
   return (
     <div>
 
         <p className="font-bold text-3xl">About Page</p>
+        <Link href="/about/address" className="bg-amber-400 rounded-3xl p-2 mt-7 text-black"> Address Page</Link>
+
+        <p>
+            <button type="button" onClick={handleNavigation} className="bg-sky-400 rounded-3xl p-2 mt-7 text-black">Address PAge</button>
+        </p>
  
     </div>
   );
